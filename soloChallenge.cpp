ecf500867b73faa7eb30885d5987eebc4df88d3f
Ethan Bitnun <ebitnun@uwaterloo.ca>
2022-01-26 22:37:16 -0500

    Working bot i hope lol

--- soloChallenge.cpp
+++ soloChallenge.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+#include <conio.h>
+#include <stdlib.h>
+#include <ctime>
 
 int main();
+void showClass(char, std::string*, std::string*, std::string*);
+void showWeapons(std::string*, std::string*, std::string*);
+void showMods(std::string*);
+void showActivity(std::string*);
+
 
 int main(){
+    // Random number testing
+    // srand(time(0));
+    // int num1 = -1;
+    // for (int i = 0 ; i < 11 ; i++){
+    //     while (num1 != i){
+    //         num1 = rand() % 11;
+    //         std::cout << num1 << std::endl;
+    //     }
+    //     std::cout << "====================== ^^ i: " << i << std::endl;
+    // }
+    
+    ////////////////////////////////////////////
     std::string * mods = new std::string [3];
     mods[0] = "Charged With Light";
     mods[1] = "Wells";
@@ -54,33 +75,79 @@ int main(){
     wClass[9] = "Attunement of Grace - Middle Tree Dawnblade";
 
     std::string * hClass = new std::string [10];
-    hClass[0] = "";
-    hClass[1] = "";
-    hClass[2] = "";
-    hClass[3] = "";
-    hClass[4] = "";
-    hClass[5] = "";
-    hClass[6] = "";
-    hClass[7] = "";
-    hClass[8] = "";
-    hClass[9] = "";
+    hClass[0] = "Way Of A Thousand Cuts - Middle Tree Gunslinger";
+    hClass[1] = "Way Of The Warrior - Top Tree Arcstrider";
+    hClass[2] = "Way Of The Outlaw - Top Tree Gunslinger";
+    hClass[3] = "Way Of The Current - Middle Tree Arcstrider";
+    hClass[4] = "Way Of The Trapper - Top Tree Nightstalker";
+    hClass[5] = "Way Of The Wraith - Middle Tree Nightstalker";
+    hClass[6] = "Way Of The Wind - Bottom Tree Arcstrider";
+    hClass[7] = "Way Of The Sharpshooter - Bottom Tree Gunslinger";
+    hClass[8] = "Revenant";
+    hClass[9] = "Way Of The Pathfinder - Bottom Tree Nightstalker";
 
     std::string * tClass = new std::string [10];
-    tClass[0] = "";
-    tClass[1] = "";
-    tClass[2] = "";
-    tClass[3] = "";
-    tClass[4] = "";
-    tClass[5] = "";
-    tClass[6] = "";
-    tClass[7] = "";
-    tClass[8] = "";
-    tClass[9] = "";
+    tClass[0] = "Code Of The Aggressor - Bottom Tree Sentinel";
+    tClass[1] = "Code Of The Devastator - Middle Tree Sunbreaker";
+    tClass[2] = "Code Of The Juggernaut - Bottom Tree Striker";
+    tClass[3] = "Code Of The Earthshaker - Top Tree Striker";
+    tClass[4] = "Code Of The Fire-Forged - Top Tree Sunbreaker";
+    tClass[5] = "Code Of The Commander - Middle Tree Sentinel";
+    tClass[6] = "Behemoth";
+    tClass[7] = "Code Of The Siegebreaker - Bottom Tree Sunbreaker";
+    tClass[8] = "Code Of The Protector - Top Tree Sentinel";
+    tClass[9] = "Code Of The Missile - Middle Tree Striker";
     /////////////////////////////////////////////////////
 
-    std::cout << "test" << std::endl;
-    char input;
-    std::cin >> input;
+    // Program loop
+    char input = 'a';
+    char exitInput = 'a';
+    while (input != 'x' && input != 'X'){
+        std::cout << "Please enter what letter corresponds to your class or enter 'x' to exit the program...\n" << std::endl;
+        std::cout << "[H]unter - [T]itan - [W]arlock" << std::endl;
+
+        // std::cin >> input;
+        input = getch();
+        // std::cout << "-" << input << "-" << std::endl;
+        input = tolower(input);
+
+        while (input != 'h' && input != 't' && input != 'w' && input != 'x'){
+
+            std::cout << "Sorry, thats not an 'H', 'T', 'W', or 'X'..." << std::endl;
+            std::cout << "Please try again..." << std::endl;
+
+            // std::cin >> input;
+            input = getch();
+            input = tolower(input);
+            system("cls");
+
+            if (!(input != 'h' && input != 't' && input != 'w' && input != 'x')){
+                break;
+            }
+        }
+
+        //std::cout << "test: " << input << std::endl;
+        if(input == 'x'){
+            break;
+        }
+
+        showClass(input, wClass, tClass, hClass);
+        showWeapons(primaries, specials, heavies);
+        showMods(mods);
+        showActivity(activity);
+
+        // Asking to exit or go again
+        std::cout << std::endl;
+        std::cout << "Would you like to roll again? Press 'X' to exit, or anything else to roll again..." << std::endl;
+        exitInput = getch();
+        exitInput = tolower(exitInput);
+        if (exitInput == 'x'){
+            break;
+        }
+        else{
+            system("cls");
+        }
+    }
 
     /////////////////////////////////////////////////////
     delete[] mods;
@@ -99,4 +166,48 @@ int main(){
     wClass = nullptr;
     hClass = nullptr;
     tClass = nullptr;
+}
+
+void showClass(char input, std::string* wClass, std::string* tClass, std::string* hClass){
+    int num = rand() % 10;
+    std::cout << "Your subclass today will be: ";
+    if (input == 'w'){
+        std::cout << wClass[num] << std::endl;
+    }
+    else if (input == 't'){
+        std::cout << tClass[num] << std::endl;
+    }
+    else if(input == 'h'){
+        std::cout << hClass[num] << std::endl;
+    }
+    else{
+        std::cout << "Error Finding Subclass" << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void showWeapons(std::string* primaries, std::string* specials, std::string* heavies){
+
+    int primaryNum = rand() % 6;
+    int specialNum = rand() % 6;
+    int heavyNum = rand() % 6;
+
+    std::cout << "Today your PRIMARY weapon will be a: " << primaries[primaryNum] << std::endl;
+    std::cout << "Today your SPECIAL weapon will be a: " << specials[specialNum] << std::endl;
+    std::cout << "Today your HEAVY weapon will be a: " << heavies[heavyNum] << std::endl;
+    std::cout << std::endl;
+}
+
+void showMods(std::string* mods){
+    int num = rand () % 3;
+    std::cout << "Today your mods will be ONLY: " << mods[num] << std::endl;
+    if (num == 0){
+        std::cout << "NO PROTECTIVE" << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void showActivity(std::string* activities){
+    int num = rand() % 6;
+    std::cout << "Today your activity will be: " << activities[num] << std::endl;
 }
